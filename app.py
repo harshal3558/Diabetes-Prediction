203a521a18f206bfa39872f69e2714c75a3d92ee
Harshal <harshal3558@gmail.com>
2025-08-13 22:56:48 +0530

    updated_1

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 from src.DIAP.logger import logging
+from src.DIAP.exception import CustomException
+from src.DIAP.components.data_ingestion import DataIngestion
+from src.DIAP.components.data_ingestion import DataIngestionConfig
+
+import sys
 
 if __name__ == "__main__":
     logging.info("the execution has started")
+
+    try:
+        data_ingestion = DataIngestion()
+        data_ingestion = initiate_data_ingestion()
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
