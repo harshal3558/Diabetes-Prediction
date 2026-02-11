af5750db0ee6befd8d1b54726a311a3a5ec6c42e
Harshal <harshal3558@gmail.com>
2026-02-12 04:23:51 +0530

    artifacts uncommented

--- src/DIAP/pipelines/prediction_pipeline.py
+++ src/DIAP/pipelines/prediction_pipeline.py
@@ -1,29 +1,40 @@
 import sys
+import os
 import pandas as pd
 from src.DIAP.exception import CustomException
 from src.DIAP.utils import load_object
-import os
 
 
 class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self,features):
+    def predict(self, features):
         try:
-            model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-            print("Before Loading")
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
-            print("After Loading")
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
-            return preds
-        
-        except Exception as e:
-            raise CustomException(e,sys)
+            # Absolute path inside Docker
+            base_path = os.getcwd()
 
+            model_path = os.path.join(base_path, "artifacts", "model.pkl")
+            preprocessor_path = os.path.join(base_path, "artifacts", "preprocessor.pkl")
+
+            print("Current Working Directory:", base_path)
+            print("Model Path:", model_path)
+            print("Preprocessor Path:", preprocessor_path)
+            print("Before Loading")
+
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+
+            print("After Loading")
+
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+
+            return preds
+
+        except Exception as e:
+            print("ERROR:", str(e))
+            raise CustomException(e, sys)
 
 
 import pandas as pd
